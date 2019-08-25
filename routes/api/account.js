6aa71f532754e00382baa0e8544d2581f3b9c662
Anish160220 <anishkalpakunj@gmail.com>
2019-08-25 14:43:19 -0700

    Create Backend For Register

---
+++ routes/api/account.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+const user=require('../../controller/user');
+
+
+// for register
+router.post('/register',user.register);
+
+
+
+module.exports = router;
